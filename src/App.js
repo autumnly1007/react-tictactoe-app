9f3b73e557cecd359ddcb23256be242410d844fd
autumnly1007 <autumnly1007@gmail.com>
2022-12-07 21:54:12 +0900

    Add Square component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import './App.css';
+import Board from './components/Board';
 
-function App() {
-  return (
-    <div className='game'>
-      <div className='game-board'></div>
-      <div className='game-info'></div>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        game
+        <div className='game-board'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          game-info
+          <div className='status'></div>
+          <ol className='todo'></ol>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
